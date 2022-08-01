1dda61a47ab7ef67901cac3b31e5071b0d29250f
ROHIT SANJAY PATIL <99815954+Rohitpatil-123@users.noreply.github.com>
2022-08-01 15:50:39 +0530

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,22 @@
+function add()
+{
+f.b3.value=eval(f.b1.value)+eval(f.b2.value)
+}
+function sub()
+{
+f.b3.value=eval(f.b1.value)-eval(f.b2.value)
+}
+function mul()
+{
+f.b3.value=eval(f.b1.value)*eval(f.b2.value)
+}
+function div()
+{
+f.b3.value=eval(f.b1.value)/eval(f.b2.value)
+}
+function clr()
+{
+f.b3.value=""
+f.b1.value=""
+f.b2.value=""
+}
